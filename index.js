668734bd84ac88ca8f56a2ed0cf11095963669c2
Uesli Laska <uesli.laska@gmail.com>
2023-08-04 16:37:04 +0200

    BetaPlan Assigments

---
+++ index.js
@@ -0,0 +1,80 @@
+//Always Hungry
+function alwaysHungry(arr) {
+    for(let i=0;i<=arr.length;i++){
+        if(arr[i]=='food'){
+            console.log("yummy");
+        }
+        else{
+            console.log("Im hungry")
+        }
+    } 
+}
+alwaysHungry([3.14, "food", "pie", true, "food"]);
+
+alwaysHungry([4, 1, 5, 7, 2]);
+
+//High Pass Filter
+function highPass(arr, cutoff) {
+    var filteredArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > cutoff) {
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
+}
+
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log(result);
+
+//Better than average
+
+function betterThanAverage(arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    
+    var average = sum / arr.length;
+    var count = 0;
+
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > average) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
+console.log(result); 
+
+//Reverse
+function reverse(arr) {
+    var reversedArr = [];
+    for (var i = arr.length - 1; i >= 0; i--) {
+        reversedArr.push(arr[i]);
+    }
+    return reversedArr;
+}
+
+var result = reverse(["a", "b", "c", "d", "e"]);
+console.log(result); // Output: ["e", "d", "c", "b", "a"]
+
+
+
+
+//Fibbonaci
+function fibonacciArray(n) {
+    var fibArr = [0, 1];
+
+    for (var i = 2; i < n; i++) {
+        fibArr.push(fibArr[i - 1] + fibArr[i - 2]);
+    }
+
+    return fibArr;
+}
+
+var result = fibonacciArray(10);
+console.log(result); // Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
